f67fa6f4eb3a21d5c416fde6fa3cb00d0138a85a
MartaAnastazja <mcm.czarnecka@gmail.com>
2021-04-17 18:24:20 +0200

    Initial Commit - pure js, css

---
+++ js/GridGenerator.js
@@ -0,0 +1,44 @@
+class GridGenerator {
+
+  generateVisualDisplay = grid => {
+    const placeholderGridElement = document.querySelector('.cag-grid');
+    const boxAlreadyExists = document.querySelector(".box");
+
+    if (boxAlreadyExists) {
+      boxAlreadyExists.parentNode.removeChild(boxAlreadyExists);
+    }
+
+    const container = document.createElement("div");
+    container.classList.add('box');
+    placeholderGridElement.style.width = `${grid.length * 40}px`;
+    placeholderGridElement.style.height = `${grid.length * 42}px`;
+    for (let i = 0; i <= grid.length - 1; i++) {
+      const row = [];
+      const rowElement = document.createElement("div");
+      rowElement.classList.add("row");
+      for (let j = 0; j <= grid[i].length - 1; j++) {
+        const element = document.createElement("div");
+        element.classList.add('box_element');
+        element.textContent = grid[i][j];
+        if (grid[i][j] === 1) {
+          const className = (grid[i][j] === 1 ? 'box_element--filled' : '');
+          element.dataset.isFilled = true;
+          element.dataset.position = [i, j];
+          element.tabIndex = 0;
+          element.classList.add(className);
+        }
+        row.push(element);
+      }
+      row.map(el => rowElement.append(el));
+      container.append(rowElement);
+    }
+    placeholderGridElement.appendChild(container);
+  }
+
+  generateGrid = () => {
+    const grid = createTwoDimenensionalArray(sizeSlider.value,sizeSlider.value);
+    this.generateVisualDisplay(grid);
+    return grid;
+  }
+
+}
---
+++ js/color-pickers.js
@@ -0,0 +1,31 @@
+const colorPickerForBoxes = document.getElementById("colorPickerForBoxes");
+const colorPickerForFilledBoxes = document.getElementById("colorPickerForFilledBoxes");
+const colorPickerForHoveredAndNeigbouringBoxes = document.getElementById("colorPickerForHoveredAndNeigbouringBoxes");
+
+const assignColorToBoxes = (boxesToColor, color) => {
+  [...document.querySelectorAll(boxesToColor)].map(box => {
+    box.style.backgroundColor = color;
+  });
+}
+
+const colorCurrentElementAndVericallyAndHorizontallyAlignedBoxes = (element, color, elementsToColorOnHover) => {
+  element.style.backgroundColor = color; 
+  const horizontallyAndVericallyAlignedBoxes = findHorizontallyAndVerticallyAlignedBoxes(element);
+  elementsToColorOnHover.forEach(elementToColorOnHover => {
+    horizontallyAndVericallyAlignedBoxes.map( alignedBoxToElement => {
+      if (alignedBoxToElement[0] === parseStringToNumber(elementToColorOnHover.dataset.position[0])
+        && alignedBoxToElement[1] === parseStringToNumber(elementToColorOnHover.dataset.position[2])
+      ) {
+        elementToColorOnHover.style.backgroundColor = color;
+      }
+    })
+  })
+}
+
+const addChangeableColorOnHover = filledBoxesToColorOnHover => {
+  const elementsToColorOnHover = [...document.querySelectorAll(filledBoxesToColorOnHover)];
+  elementsToColorOnHover.map(box => {
+    box.addEventListener('mouseenter', e => colorCurrentElementAndVericallyAndHorizontallyAlignedBoxes(e.target, colorPickerForHoveredAndNeigbouringBoxes.value, elementsToColorOnHover));
+    box.addEventListener('mouseleave', e => colorCurrentElementAndVericallyAndHorizontallyAlignedBoxes(e.target, colorPickerForFilledBoxes.value, elementsToColorOnHover));
+  });
+}
